dacbe74661b9cedf38f5cae5e0733ecbaddac847
ekathuria <e@kathuria.org>
2019-11-07 14:05:19 -0500

    Update icons and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,11 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="https://github.ekathuria.com" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="https://linkedin.com/in/ekathuria" target="_blank" rel="noopener noreferrer" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                        <li><a href="https://www.meetup.com/members/56793422/" target="_blank" rel="noopener noreferrer" className="icon fa-meetup"><span className="label">Meetup</span></a></li>
+                        <li><a href="https://www.last.fm/user/ekathuria" target="_blank" rel="noopener noreferrer" className="icon fa-lastfm"><span className="label">LastFM</span></a></li>
+                        <li><a href="https://open.spotify.com/user/ebo.one" target="_blank" rel="noopener noreferrer" className="icon fa-spotify"><span className="label">Spotify</span></a></li>
                     </ul>
                 </div>
             </div>
