5187a2876e6c1369d14c0bc3261048c74b37e67e
ekathuria <e@kathuria.org>
2019-11-10 20:04:47 -0500

    Update Font Awesome to version 5

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,12 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="https://github.ekathuria.com" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="https://linkedin.com/in/ekathuria" target="_blank" rel="noopener noreferrer" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
-                        <li><a href="https://www.meetup.com/members/56793422/" target="_blank" rel="noopener noreferrer" className="icon fa-meetup"><span className="label">Meetup</span></a></li>
-                        <li><a href="https://www.last.fm/user/ekathuria" target="_blank" rel="noopener noreferrer" className="icon fa-lastfm"><span className="label">LastFM</span></a></li>
-                        <li><a href="https://open.spotify.com/user/ebo.one" target="_blank" rel="noopener noreferrer" className="icon fa-spotify"><span className="label">Spotify</span></a></li>
+                        <li><a href="https://github.ekathuria.com" className="fab fa-github"></a></li>
+                        <li><a href="https://linkedin.com/in/ekathuria" target="_blank" rel="noopener noreferrer" className="fab fa-linkedin"></a></li>
+                        <li><a href="https://www.meetup.com/members/56793422/" target="_blank" rel="noopener noreferrer" className="fab fa-meetup"><span className="label">Meetup</span></a></li>
+                        <li><a href="https://www.last.fm/user/ekathuria" target="_blank" rel="noopener noreferrer" className="fab fa-lastfm"><span className="label">LastFM</span></a></li>
+                        <li><a href="https://open.spotify.com/user/ebo.one" target="_blank" rel="noopener noreferrer" className="fab fa-spotify"><span className="label">Spotify</span></a></li>
+                        <li><a href="https://goodreads.com/haikutear" target="_blank" rel="noopener noreferrer" className="fab fa-goodreads"><span className="label">Goodreads</span></a></li>
                     </ul>
                 </div>
             </div>
