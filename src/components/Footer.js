233e1f2e359959e006c9359254ece6bc31e4073c
E Kathuria <e@looklisten.com>
2020-10-07 08:05:07 -0400

    Update link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <div className="icons">
-                        <a href="https://github.ekathuria.com" className="fab fa-github"><span className="label">Github</span></a>
+                        <a href="https://github.com/ekathuria" className="fab fa-github"><span className="label">Github</span></a>
                         <a href="https://www.notion.so/Reading-List-923600ed9afc4db9b7cb8f99dcda99ed" target="_blank" rel="noopener noreferrer" className="fas fa-glasses"><span className="label">Books</span></a>
                         <a href="https://open.spotify.com/user/ebo.one" target="_blank" rel="noopener noreferrer" className="fab fa-spotify"><span className="label">Spotify</span></a>
                         <a href="https://www.meetup.com/members/56793422/" target="_blank" rel="noopener noreferrer" className="fab fa-meetup"><span className="label">Meetup</span></a>
